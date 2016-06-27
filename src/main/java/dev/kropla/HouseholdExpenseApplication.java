5e1404b1ab37c1e6907dbbd36d66dd3c48be9692
kropladev <makroplewski@gmail.com>
2016-06-27 15:18:16 +0200

    init the repo

---
+++ src/main/java/dev/kropla/HouseholdExpenseApplication.java
@@ -0,0 +1,12 @@
+package dev.kropla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdExpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdExpenseApplication.class, args);
+	}
+}
